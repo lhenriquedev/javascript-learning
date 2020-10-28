a8a142dea2102643e3d7e18b649f35079fe93e75
Henrique Marques <72282029+lhenriquedev@users.noreply.github.com>
2020-10-27 21:25:15 -0300

    challenges and sections

--- JavaScript Fundamentals - part I/main.js
+++ JavaScript Fundamentals - part I/main.js
@@ -22,16 +22,64 @@ console.log(Boolean([]));
 
 const money = 0;
 if (money) {
-  console.log(`Don't spend it all :)`);
+  // console.log(`Don't spend it all :)`);
 } else {
-  console.log(`You should get a job!`);
+  // console.log(`You should get a job!`);
 }
 
 let height; // falsy
 if (height) {
-  console.log(`Yay! Height is defined`);
+  // console.log(`Yay! Height is defined`);
 } else {
-  console.log(`Height is UNDEFINED`);
+  // console.log(`Height is UNDEFINED`);
 }
 
-// Equality operator
+// Equality operator -> return true or false -> strict and loose
+// const favourite = Number(prompt(`What is your favourite number?`));
+// if (favourite === 23) console.log('Yeeees, it is 23.');
+
+// Different
+// if (favourite !== 23) console.log(`Nooo, it is not 23.`);
+
+// Boolean Logic
+const hasDriversLicense = true;
+const hasGoodVision = true;
+
+// if (hasDriversLicense && hasGoodVision) {
+//   console.log(`Sarah is able to drive!`);
+// } else {
+//   console.log(`Someone else should drive...`);
+// }
+
+const isTired = true; // isTired = true, !isTired = false;
+
+if (hasDriversLicense && hasGoodVision && !isTired) {
+  // console.log(`Sarah is able to drive!`);
+} else {
+  // console.log(`Someone else should drive...`);
+}
+
+// Switch case
+const day = 'monday';
+
+switch (day) {
+  case 'monday': // day === 'monday'
+    // console.log('Plan course structure');
+    break;
+  case 'tuesday':
+    // console.log('Prepare theory videos');
+    break;
+  default:
+  // console.log('Not a valid day!');
+}
+
+// Statements and Expressions
+
+// Expression -> it is a piece of code that produces a value
+// Statements -> not produces a value itself
+
+// Conditional Operator -> Ternary
+// const age = 23;
+// const fullAge = age
+//   ? console.log(`I like to drink wine 🍷`)
+//   : console.log(`I like to drink water 🌊`);
--- JavaScript Fundamentals - part I/script.js
+++ JavaScript Fundamentals - part I/script.js
@@ -1,9 +1,10 @@
 /* 
 ------------
-Coding Challenge #1 and #2
+Coding Challenge #1, #2, #3, #4
 ------------
 */
 
+/*
 const markMass = 78;
 const markHeight = 1.69;
 
@@ -22,3 +23,36 @@ if (bmiMark > bmiJohn) {
 } else {
   console.log(`John's BMI is higher than Mark's`);
 }
+
+
+const dolphinsScore = 97 + 112 + 80;
+const koalasScore = 109 + 95 + 50;
+
+const averageDolphins = dolphinsScore / 3;
+const averageKoalas = koalasScore / 3;
+
+const minimumScore = 100;
+
+if (averageDolphins > averageKoalas && averageDolphins > minimumScore) {
+  // console.log(`Dolphin is the winner!`);
+} else if (averageKoalas > averageDolphins && averageKoalas > minimumScore) {
+  // console.log(`Koalas are the winner!`);
+} else if (
+  averageKoalas === averageKoalas &&
+  averageDolphins &&
+  averageKoalas > 100
+) {
+  // console.log(`There was a draw`);
+} else {
+  // console.log(`NOBODY wins.`);
+}
+*/
+
+const bill = 40;
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+const total = bill + tip;
+
+console.log(
+  `The bill was ${bill}, the tip was ${tip} and the total value is ${total}`
+);
