c3136b9562b2f413ea090231ac55ee4f3734d520
Henrique Marques <72282029+lhenriquedev@users.noreply.github.com>
2020-11-27 15:39:46 -0300

    for of

--- Data Structure/challenge.js
+++ Data Structure/challenge.js
@@ -1,63 +1,63 @@
-const game = {
-  team1: 'Bayern Munich',
-  team2: 'Borrussia Dortmund',
-  players: [
-    [
-      'Neuer',
-      'Pavard',
-      'Martinez',
-      'Alaba',
-      'Davies',
-      'Kimmich',
-      'Goretzka',
-      'Coman',
-      'Muller',
-      'Gnarby',
-      'Lewandowski',
-    ],
-    [
-      'Burki',
-      'Schulz',
-      'Hummels',
-      'Akanji',
-      'Hakimi',
-      'Weigl',
-      'Witsel',
-      'Hazard',
-      'Brandt',
-      'Sancho',
-      'Gotze',
-    ],
-  ],
-  score: '4:0',
-  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
-  date: 'Nov 9th, 2037',
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
-  },
-};
+// const game = {
+//   team1: 'Bayern Munich',
+//   team2: 'Borrussia Dortmund',
+//   players: [
+//     [
+//       'Neuer',
+//       'Pavard',
+//       'Martinez',
+//       'Alaba',
+//       'Davies',
+//       'Kimmich',
+//       'Goretzka',
+//       'Coman',
+//       'Muller',
+//       'Gnarby',
+//       'Lewandowski',
+//     ],
+//     [
+//       'Burki',
+//       'Schulz',
+//       'Hummels',
+//       'Akanji',
+//       'Hakimi',
+//       'Weigl',
+//       'Witsel',
+//       'Hazard',
+//       'Brandt',
+//       'Sancho',
+//       'Gotze',
+//     ],
+//   ],
+//   score: '4:0',
+//   scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+//   date: 'Nov 9th, 2037',
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+// };
 
-const [players1, players2] = game.players; // 1) ✅
-console.log(players1, players2);
+// const [players1, players2] = game.players; // 1) ✅
+// console.log(players1, players2);
 
-const [gk, ...fieldPlayers] = players1; // 2) ✅
-console.log(gk, fieldPlayers);
+// const [gk, ...fieldPlayers] = players1; // 2) ✅
+// console.log(gk, fieldPlayers);
 
-const allPlayers = [...players1, ...players2]; // 3) ✅
-console.log(allPlayers);
+// const allPlayers = [...players1, ...players2]; // 3) ✅
+// console.log(allPlayers);
 
-const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic']; // 4) ✅
-console.log(players1Final);
+// const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic']; // 4) ✅
+// console.log(players1Final);
 
-const { team1, x: draw, team2 } = game.odds; // 5) ✅
-console.log(team1, draw, team2);
+// const { team1, x: draw, team2 } = game.odds; // 5) ✅
+// console.log(team1, draw, team2);
 
-const printGoals = (...numPlayersNames) => {
-  console.log(`${numPlayersNames.length} goals were scored`);
-};
-printGoals(...game.scored);
-// printGoals('Davies', 'Muller');
+// const printGoals = (...numPlayersNames) => {
+//   console.log(`${numPlayersNames.length} goals were scored`);
+// };
+// printGoals(...game.scored);
+// // printGoals('Davies', 'Muller');
 
-team1 < team2 && console.log('Team1 is more likely to win.');
+// team1 < team2 && console.log('Team1 is more likely to win.');
--- Data Structure/script.js
+++ Data Structure/script.js
@@ -1,52 +1,52 @@
 // 'use strict';
 
-// const restaurant = {
-//   name: 'Classico Italiano',
-//   location: 'Via Angelo Tavanti 23, Firenze, Italy',
-//   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-//   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-//   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
 
-//   openingHours: {
-//     thu: {
-//       open: 12,
-//       close: 22,
-//     },
-//     fri: {
-//       open: 11,
-//       close: 23,
-//     },
-//     sat: {
-//       open: 0, // Open 24 hours
-//       close: 24,
-//     },
-//   },
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
 
-//   order: function (starterIndex, mainIndex) {
-//     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-//   },
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
 
-//   // Destructuring properties
-//   orderDelivery: function ({
-//     starterIndex = 1,
-//     mainIndex = 0,
-//     time = '20:00',
-//     address,
-//   }) {
-//     console.log(
-//       `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
-//     );
-//   },
+  // Destructuring properties
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = "20:00",
+    address,
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
 
-//   orderPasta: function (ing1, ing2, ing3) {
-//     console.log(`Here is your delicious pasta with: ${ing1}, ${ing2}, ${ing3}`);
-//   },
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`Here is your delicious pasta with: ${ing1}, ${ing2}, ${ing3}`);
+  },
 
-//   orderPizza: function (mainIngredient, ...otherIngredient) {
-//     console.log(mainIngredient);
-//     console.log(otherIngredient);
-//   },
-// };
+  orderPizza: function (mainIngredient, ...otherIngredient) {
+    console.log(mainIngredient);
+    console.log(otherIngredient);
+  },
+};
 
 // restaurant.orderDelivery({
 //   time: '22:30',
@@ -219,3 +219,13 @@
 // // Nullish: null and undefined (NOT 0 or '')
 // const guestCorret = restaurant.numGuests ?? 10;
 // console.log(guestCorret);
+
+// for of
+const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
+for (const item of menu) {
+  // console.log(item);
+}
+
+for (const [i, el] of menu.entries()) {
+  console.log(`${i + 1} - ${el}`);
+}
