0d26df8de4786f4e345b3f82d0db71e6ff4cdbf7
henriquedev <lhenrique.dev@gmail.com>
2020-12-23 17:57:54 -0300

    login functionality and some stuffs :fire:

--- Arrays - Bankist/assets/js/challenge.js
+++ Arrays - Bankist/assets/js/challenge.js
@@ -46,15 +46,22 @@ TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
 GOOD LUCK 😀
 */
 
-const calcAverageHumanAge = ages => {
-  const humanAge = ages.map(age => (age <= 2 ? age * 2 : 16 + age * 4));
-  const adults = humanAge.filter(age => age >= 18);
-  const average = adults.reduce(
-    (acc, age, _, arr) => acc + age / arr.length,
-    0
-  );
+// const calcAverageHumanAge = ages => {
+//   const humanAge = ages.map(age => (age <= 2 ? age * 2 : 16 + age * 4));
+//   const adults = humanAge.filter(age => age >= 18);
+//   const average = adults.reduce(
+//     (acc, age, _, arr) => acc + age / arr.length,
+//     0
+//   );
 
-  return average;
+//   return average;
+// };
+
+const calcAverageHumanAge = ages => {
+  return ages
+    .map(age => (age <= 2 ? age * 2 : 16 + age * 4))
+    .filter(age => age >= 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
 };
 
 const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
---
+++ Arrays - Bankist/assets/js/script.js
@@ -0,0 +1,161 @@
+'use strict';
+
+/////////////////////////////////////////////////
+// BANKIST APP
+
+// Elements
+const labelWelcome = document.querySelector('.welcome');
+const labelDate = document.querySelector('.date');
+const labelBalance = document.querySelector('.balance__value');
+const labelSumIn = document.querySelector('.summary__value--in');
+const labelSumOut = document.querySelector('.summary__value--out');
+const labelSumInterest = document.querySelector('.summary__value--interest');
+const labelTimer = document.querySelector('.timer');
+
+const containerApp = document.querySelector('.app');
+const containerMovements = document.querySelector('.movements');
+
+const btnLogin = document.querySelector('.login__btn');
+const btnTransfer = document.querySelector('.form__btn--transfer');
+const btnLoan = document.querySelector('.form__btn--loan');
+const btnClose = document.querySelector('.form__btn--close');
+const btnSort = document.querySelector('.btn--sort');
+
+const inputLoginUsername = document.querySelector('.login__input--user');
+const inputLoginPin = document.querySelector('.login__input--pin');
+const inputTransferTo = document.querySelector('.form__input--to');
+const inputTransferAmount = document.querySelector('.form__input--amount');
+const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+const inputCloseUsername = document.querySelector('.form__input--user');
+const inputClosePin = document.querySelector('.form__input--pin');
+
+// Data
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: 'Steven Thomas Williams',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: 'Sarah Smith',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+
+const displayMovements = movements => {
+  // reset html
+  containerMovements.innerHTML = '';
+
+  // create deposit and withdrawal
+  movements.forEach((mov, i) => {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">
+        ${i + 1} ${type}</div>
+        <div class="movements__value">R$${Math.abs(mov)}</div>
+      </div>
+    `;
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+const displayBalance = movements => {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  // -> R$3400
+  labelBalance.textContent = `R$${balance}`;
+};
+
+const displaySummary = acc => {
+  const incomes = acc.movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+
+  //  only incomes -> R$+16323
+  labelSumIn.textContent = `R$${incomes}`;
+
+  const outs = acc.movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  // only outs -> R$-16000
+  labelSumOut.textContent = `R$${Math.abs(outs)}`;
+
+  const interest = acc.movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * acc.interestRate) / 100)
+    .filter((int, i, arr) => int >= 1)
+    .reduce((acc, int) => acc + int, 0);
+
+  // only interest is deposit * interest -> %
+  labelSumInterest.textContent = `R$${interest}`;
+};
+
+const createUsernames = accs => {
+  accs.forEach(acc => {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+
+  // Steven Thomas Williams -> stw
+};
+
+createUsernames(accounts);
+
+/* variables states */
+let currentAccount;
+
+btnLogin.addEventListener('click', e => {
+  e.preventDefault();
+
+  // Procura se existe conta digitada
+  currentAccount = accounts.find(
+    acc => acc.username === inputLoginUsername.value
+  );
+
+  // caso usuario digite certo
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    // Display UI and Message
+    labelWelcome.textContent = `Welcome back, ${
+      currentAccount.owner.split(' ')[0]
+    }`;
+    containerApp.style.opacity = 100;
+
+    // Clear the input fields
+    inputLoginUsername.value = '';
+    inputLoginPin.value = '';
+
+    inputLoginPin.blur();
+
+    // Display movements
+    displayMovements(currentAccount.movements);
+    // Display balance
+    displayBalance(currentAccount.movements);
+    // Display summary
+    displaySummary(currentAccount);
+  }
+});
