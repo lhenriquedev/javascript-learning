1ce434915931c6bc2bbf7523bd91abed7bedea2f
Henrique Marques <72282029+lhenriquedev@users.noreply.github.com>
2021-01-11 17:39:12 -0300

    challenge #2 :fire:

--- Arrays - Bankist/assets/js/challenge.js
+++ Arrays - Bankist/assets/js/challenge.js
@@ -25,9 +25,9 @@ const checkDogs = (dogsJulia, dogsKate) => {
 
   dogs.forEach((dog, i) => {
     if (dog >= 3) {
-      console.log(`Dog number ${i + 1} is an adult, and is ${dog} years old`);
+      // console.log(`Dog number ${i + 1} is an adult, and is ${dog} years old`);
     } else {
-      console.log(`Dog number ${i + 1} is still a puppy 🐶`);
+      // console.log(`Dog number ${i + 1} is still a puppy 🐶`);
     }
   });
 };
@@ -57,6 +57,23 @@ TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
 TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
 GOOD LUCK 😀
 */
+
+const calcAverageHumanAge = ages => {
+  const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
+  console.log(humanAges);
+  const adults = humanAges.filter(age => age >= 18);
+
+  const average = adults.reduce((acc, age, i, arr) => {
+    return acc + age / arr.length;
+  }, 0);
+
+  return average;
+};
+
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+
+console.log(avg1, avg2);
 
 // Coding Challenge #4
 
