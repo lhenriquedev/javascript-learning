3f7dd68d405521eb9642f0f663b65de3ad86edbf
Henrique <lhenrique.dev@gmail.com>
2021-01-19 22:24:07 -0300

    finished challenge #4 :fire:

--- Arrays - Bankist/assets/js/challenge.js
+++ Arrays - Bankist/assets/js/challenge.js
@@ -103,40 +103,74 @@ const dogs = [
   { weight: 32, curFood: 340, owners: ['Michael'] },
 ];
 
-dogs.forEach(dog => {
-  dog.recommendedFood = Math.trunc(dog.weight ** 0.75 * 28);
-});
+dogs.forEach(
+  dog => (dog.recommendedFood = Math.trunc(dog.weight ** 0.75 * 28))
+);
 
-console.log(dogs);
 /*
 2. Find Sarah's dog and log to the console whether it's eating too much or too little. HINT: Some dogs have multiple owners, so you first need to find Sarah in the owners array, and so this one is a bit tricky (on purpose) 🤓
 */
+
+const dogSarah = dogs.find(dog => dog.owners.includes('Sarah'));
+console.log(
+  `Sarah's dog is eating ${
+    dogSarah.curFood > dogSarah.recommendedFood ? 'much' : 'little'
+  }.`
+);
 
 /*
 3. Create an array containing all owners of dogs who eat too much ('ownersEatTooMuch') and an array with all owners of dogs who eat too little ('ownersEatTooLittle').
 */
 
+const ownersEatTooMuch = dogs
+  .filter(dog => dog.curFood > dog.recommendedFood)
+  .flatMap(dog => dog.owners);
+
+const ownersEatTooLittle = dogs
+  .filter(dog => dog.curFood < dog.recommendedFood)
+  .flatMap(dog => dog.owners);
 /*
 4. Log a string to the console for each array created in 3., like this: "Matilda and Alice and Bob's dogs eat too much!" and "Sarah and John and Michael's dogs eat too little!"
 */
+console.log(`${ownersEatTooMuch.join(' and ')} dogs eat too much.`);
+console.log(`${ownersEatTooLittle.join(' and ')} dogs eat too little.`);
 
 /*
 5. Log to the console whether there is any dog eating EXACTLY the amount of food that is recommended (just true or false)
 */
+// console.log(dogs.some(dog => dog.recommendedFood === dog.curFood));
 
 /*
 6. Log to the console whether there is any dog eating an OKAY amount of food (just true or false)
 */
 
+// current > (recommended * 0.90) && current < (recommended * 1.10)
+console.log(
+  dogs.some(
+    dog =>
+      dog.curFood > dog.recommendedFood * 0.9 &&
+      dog.curFood < dog.recommendedFood * 1.1
+  )
+);
+
 /*
-7. Create an array containing the dogs that are eating an OKAY amount of food (try to reuse the condition used in 6.)
-/*
+7. Create an array containing the dogs that are eating an OKAY amount of food (try to reuse the condition used in 6.)*/
+
+const dogsEatingOkay = dogs.filter(
+  dog =>
+    dog.curFood > dog.recommendedFood * 0.9 &&
+    dog.curFood < dog.recommendedFood * 1.1
+);
 
 /*
 8. Create a shallow copy of the dogs array and sort it by recommended food portion in an ascending order (keep in mind that the portions are inside the array's objects)
 HINT 1: Use many different tools to solve these challenges, you can use the summary lecture to choose between them 😉
 HINT 2: Being within a range 10% above and below the recommended portion means: current > (recommended * 0.90) && current < (recommended * 1.10). Basically, the current portion should be between 90% and 110% of the recommended portion.
 */
+
+const dogsEatingSort = dogs
+  .slice()
+  .sort((a, b) => a.recommendedFood - b.recommendedFood);
 
 /*
 TEST DATA:
