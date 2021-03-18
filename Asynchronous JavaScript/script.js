f10229d0f8798bd9c00f44f3c377251a82063a03
Henrique <lhenrique.dev@gmail.com>
2021-03-18 09:44:44 -0300

    creating promises :fire:

--- Asynchronous JavaScript/script.js
+++ Asynchronous JavaScript/script.js
@@ -168,11 +168,11 @@ btn.addEventListener('click', function () {
 */
 // getCountryData('fdsafdsdsf');
 
-const whereAmI = (lat, lng) => {
+/*const whereAmI = (lat, lng) => {
   // Fetching the geolocation API
   return fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
     .then(res => {
-      if (!res.ok) throw new Error(`There was too request. ${res.status}`);
+      if (!res.ok) throw new Error(`There was too much request. ${res.status}`);
       return res.json();
     })
     .then(data => {
@@ -193,3 +193,127 @@ const whereAmI = (lat, lng) => {
 whereAmI(52.508, 13.381);
 // whereAmI(19.037, 72.837);
 // whereAmI(-33.933, 18.474);
+*/
+
+/* const lotteryPromise = new Promise((resolve, reject) => {
+  // executor
+  // console.log('Lotter draw is happening');
+  setTimeout(() => {
+    if (Math.random() >= 0.5) {
+      resolve('You win, u piece of shit!');
+    } else {
+      reject(new Error('You lost your money, fucking retard.'));
+    }
+  }, 2000);
+});
+*/
+
+// lotteryPromise.then(res => console.log(res)).catch(err => console.log(err));
+
+// Promisifying setTimeout
+
+/* const wait = seconds => {
+  return new Promise(resolve => setTimeout(resolve, seconds * 1000));
+};
+
+wait(1)
+  .then(() => {
+    console.log('Waited for 1 seconds');
+    return wait(1);
+  })
+  .then(() => console.log('Waited for 2 seconds'));
+
+Promise.resolve('Pedro gay').then(x => console.log(x));
+Promise.reject(new Error('Pedro TS modinha is on.')).catch(x => console.log(x));
+*/
+
+const getPosition = () => {
+  return new Promise((resolve, reject) => {
+    // navigator.geolocation.getCurrentPosition(
+    //   position => resolve(position),
+    //   err => reject(err)
+    // );
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
+
+const whereAmI = () => {
+  getPosition()
+    .then(pos => {
+      const { latitude: lat, longitude: lng } = pos.coords;
+      return fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+    })
+    // Fetching the geolocation API
+    .then(res => {
+      if (!res.ok) throw new Error(`There was too much request. ${res.status}`);
+      return res.json();
+    })
+    .then(data => {
+      console.log(`You are in ${data.city}, ${data.country}`);
+      return fetch(`https://restcountries.eu/rest/v2/name/${data.country}`);
+    })
+    .then(res => {
+      if (!res.ok) {
+        throw new Error(`${errorMsg} (${res.status})`);
+      }
+      return res.json();
+    })
+    .then(data => renderCountry(data[0]))
+    .catch(err =>
+      console.error(`Something went wrong. ${err.message}. Try again!`)
+    );
+};
+
+btn.addEventListener('click', whereAmI);
+
+const wait = seconds => {
+  return new Promise(resolve => setTimeout(resolve, seconds * 1000));
+};
+
+const imageContainer = document.querySelector('.images');
+
+const createImage = imagePath => {
+  return new Promise((resolve, reject) => {
+    const img = document.createElement('img');
+    img.src = imagePath;
+
+    img.addEventListener('load', () => {
+      imageContainer.append(img);
+      resolve(img);
+    });
+    img.addEventListener('error', () => {
+      reject(new Error('Image not found.'));
+    });
+  });
+};
+
+let currentImg;
+createImage('img/img-1.jpg')
+  .then(img => {
+    currentImg = img;
+    console.log('Image 1 loaded');
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+    return createImage('img/img-2.jpg');
+  })
+  .then(img => {
+    currentImg = img;
+    console.log('Image 2 loaded');
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+    return createImage('img/img-3.jpg');
+  })
+  .then(img => {
+    currentImg = img;
+    console.log('Image 3 loaded');
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+  })
+
+  .catch(err => console.log('Deu ruim', err));
